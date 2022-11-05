e7133f05676005c042e4033696946c929b253511
Lucas De Bortoli <lucasg.debortoli@gmail.com>
2022-11-05 16:01:38 -0300

    Change json for cml args

---
+++ conftest.py
@@ -0,0 +1,61 @@
+import pytest
+import json
+from selenium import webdriver
+
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        action="store",
+        default="chrome",
+        choices=("chrome", "firefox"),
+        help="Browser to run the test cases (options: chrome, firefox)"
+    )
+    parser.addoption(
+        "--headless",
+        action="store_true",
+        default=False,
+        help="Set headless mode"
+    )
+
+
+@pytest.fixture
+def args(request, scope='session'):
+    return {
+        "browser": request.config.getoption("--browser"),
+        "headless": request.config.getoption("--headless")
+    }
+
+
+@pytest.fixture
+def driver(args):
+    # Set Up driver
+    driver = browser_selection(args["browser"])
+    driver.maximize_window()
+    driver.get("https://www.google.com")
+    
+    yield driver
+
+    driver.quit()
+
+
+def browser_selection(browser):
+    if browser == "chrome":
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    elif browser == "firefox":
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    elif browser == "edge":
+        driver = None
+    elif browser == "opera":
+        driver = None
+    elif browser == "brave":
+        driver = None
+    elif browser == "chromium":
+        driver = None
+    return driver
--- src/tests/test_search_bar.py
+++ src/tests/test_search_bar.py
@@ -7,9 +7,8 @@ class BaseTest:
     pass
 
 
-#@pytest.mark.usefixtures("setup")
 class TestSearchBar(BaseTest):
 
     def test_search_bar(self, driver):
         BasePage(driver).search_word_in_bar("PC")
-        time.sleep(5)
+        time.sleep(2)
